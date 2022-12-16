fb928c896db8916c279436129b25894a96ff43a2
Rinat1588 <rinat15best@gmail.com>
2022-12-16 13:31:27 +0500

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using CarServiceL.View.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CarServiceL
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            FrameMain.Navigate(new ServicesPage());
+        }
+
+        private void BthBackClick(object sender, RoutedEventArgs e)
+        {
+            if (FrameMain.CanGoBack)
+                FrameMain.GoBack();
+
+        }
+
+        private void WindowsClosed(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnBackClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ View/Pages/ServicesPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using CarServiceL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CarServiceL.View.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ServicesPage.xaml
+    /// </summary>
+    public partial class ServicesPage : Page
+    {
+        Core db = new Core();
+        List<Service> arrayService;
+        public ServicesPage()
+        {   
+            InitializeComponent();
+            arrayService = db.context.Services.ToList();
+            LViewServices.ItemsSource = arrayService;
+        }
+
+
+
+        private void BtnBackClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAddService_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
